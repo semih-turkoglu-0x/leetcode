1529bc0050b464b167b486dac4713d6fb9fe1155
Semih Turkoglu <semih.trkgl99@gmail.com>
2026-04-05 23:19:23 +0200

    fix: fixing findTheDifference() method using XOR operator

--- src/Difference.java
+++ src/Difference.java
@@ -1,14 +1,8 @@
 public class Difference {
 	public char findTheDifference(String s, String t) {
-		int counter = 0;
-		char s1 = 'k';
-		char t1;
-		while (counter <= t.length()) {
-			if (counter < s.length()) s1 = s.charAt(counter);
-			t1 = t.charAt(counter);
-			if (t1 != s1) return t1;
-			counter ++;
-		}
-		return 'a';
+		char res = 0;
+		for(char cs : s.toCharArray()) res ^= cs;
+		for(char ct : t.toCharArray()) res ^= ct;
+		return res;
 	}
 }
