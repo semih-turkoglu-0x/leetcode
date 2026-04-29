9a13be175c4bf2af3707de899f09e6f7a1c385d8
Semih Turkoglu <semih.trkgl99@gmail.com>
2026-04-29 11:17:51 +0200

    adding FindMaxConsecutiveOnes

---
+++ src/FindMaxConsecutiveOnes.java
@@ -0,0 +1,17 @@
+public class FindMaxConsecutiveOnes {
+	public int findMaxConsecutiveOnes(int[] nums) {
+		int current = 0;
+		int max = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1) {
+				current++;
+			} else {
+				current = 0;
+			}
+			if (current > max) {
+				max = current;
+			}
+		}
+		return max;
+	}
+}
